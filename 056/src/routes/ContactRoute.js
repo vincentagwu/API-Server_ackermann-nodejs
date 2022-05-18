d3d8b287c9d8b5885ce132d354a9dfdf7da3cbfc
Vincent Agwu <vincentagwu@gmail.com>
2022-05-18 12:57:53 +0200

    Parsed search params correct+filtering outputarray

--- 056/src/routes/ContactRoute.js
+++ 056/src/routes/ContactRoute.js
@@ -1,16 +1,41 @@
 const ContactModel = require('../model/ContactModel')
-const qs = require('qs');
-const assert = require('assert');
+const qs = require('qs')
 
 module.exports = class ContactRoute {
   static getContacts (request, response) {
-    const filter = JSON.parse(JSON.stringify(qs.parse(request.query, { delimiter: '?' })))
-    console.log(JSON.parse(JSON.stringify(qs.parse(request.query, { delimiter: '?' }))))
-    //var obj = JSON.parse(filter);
+    // Initialize Variables
+    let firstNameFilter, lastNameFilter, contactsArray
 
-    // Accessing individual value from JS object
-    //console.log(obj.firstName); // Outputs: Peter
-    const contactsArray = ContactModel.getContacts()
+    // Variable which is containing the request with the search params
+    const filter = qs.parse(request.query, { delimiter: '?' })
+
+    // Condition, which checks if the request contains a param firstname or a part of a firstname
+    if (typeof filter.firstName === 'object') { firstNameFilter = filter.firstName.contains } else if (filter.firstName !== undefined || filter.firstName !== null) { firstNameFilter = filter.firstName }
+
+    // Checks if the request contains a param lastname or a part of a lastname
+    if (typeof filter.lastName === 'object') { lastNameFilter = filter.lastName.contains } else if (filter.lastName !== undefined || filter.lastName !== null) { lastNameFilter = filter.lastName }
+
+    // Checks if the previous condition have given the variables values
+    if (firstNameFilter === undefined && lastNameFilter === undefined) {
+      // No search param was given with
+      contactsArray = ContactModel.getContacts()
+    } else if (lastNameFilter === undefined) {
+      // Only the firstname will be searched after the given search param
+      contactsArray = ContactModel.getContacts().filter((contact) => {
+        return (contact.firstName.includes(firstNameFilter))
+      })
+    } else if (firstNameFilter === undefined) {
+      // Only the lastName will be searched after the given search param
+      contactsArray = ContactModel.getContacts().filter((contact) => {
+        return (contact.lastName.includes(lastNameFilter))
+      })
+    } else {
+      contactsArray = ContactModel.getContacts().filter((contact) => {
+        // Firstname and lastName will be searched after the given search param
+        return (contact.firstName.includes(firstNameFilter) && contact.lastName.includes(lastNameFilter))
+      })
+    }
+
     response.send(contactsArray)
   }
 
